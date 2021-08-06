b459b5c5a7778cb6466a78e42929d3b5f5001d7a
GeetanjaliS88 <68696722+GeetanjaliS88@users.noreply.github.com>
2021-08-06 11:14:31 +0530

    Final

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void mars(float radius);
 
 float i,j,count=0,count1=0,count3=0,flag=0,flag1=0,t=0,f=0,flag3=0;
 
-// fucntion to display the text content of the home screen
+
 void drawstring(int x, int y, char *s)
 {
 	char *c;
@@ -42,13 +42,13 @@ void semicircle(float radius,float u,float v)
    for (int i=135; i<=315; i++)
    {
       float degInRad = i*DEG2RAD;
-      glVertex2f(u+cos(degInRad)*radius,v+(sin(degInRad))*radius);//100,100 specifies centre of the circle
+      glVertex2f(u+cos(degInRad)*radius,v+(sin(degInRad))*radius);
    }
 
    glEnd();
 }
 
-//determines the state of rocket launch
+
 void control()
 {
 	count1++;
@@ -195,20 +195,20 @@ flag=1;
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 	glColor3f(0.4,0.25,0.1);
-		glBegin(GL_POLYGON);//green ground
+		glBegin(GL_POLYGON);
 		glVertex2f(0.0,0.0);
 		glVertex2f(0.0,250.0);
 		glVertex2f(270.0,250.0);
 		glVertex2f(500.0,50.0);
 		glVertex2f(500.0,0.0);
 		glEnd();
-		glBegin(GL_POLYGON);//green ground
+		glBegin(GL_POLYGON);
 		glVertex2f(280.0,250.0);
 		glVertex2f(500.0,250.0);
 		glVertex2f(500.0,60.0);
 		glEnd();
 		glColor3f(0.0,0.0,0.0);
-			glBegin(GL_POLYGON);//road
+			glBegin(GL_POLYGON);
 		glVertex2f(260.0,250.0);
 		glVertex2f(290.0,250.0);
 		glVertex2f(500.0,70.0);
@@ -218,69 +218,69 @@ flag=1;
 
 
 		glColor3f(0.8,0.498039 ,0.196078);
-			glBegin(GL_POLYGON);//house 1
+			glBegin(GL_POLYGON);
 		glVertex2f(250.0,250.0);
 		glVertex2f(300.0,250.0);
 		glVertex2f(300.0,350.0);
 		glVertex2f(250.0,350.0);
 		glEnd();
 		glColor3f(0.7,0.7,0.7);
-		glBegin(GL_POLYGON);//HOUSE A
+		glBegin(GL_POLYGON);
 			glVertex2f(255,267.5);
 			glVertex2f(275.0,267.5);
 			glVertex2f(275.0,277.5);
 			glVertex2f(255.0,277.5);
 			glEnd();
-		glBegin(GL_POLYGON);//HOUSE B
+		glBegin(GL_POLYGON);
 			glVertex2f(255,285.0);
 			glVertex2f(275.0,285);
 			glVertex2f(275.0,295);
 			glVertex2f(255.0,295);
 			glEnd();
 
-		glBegin(GL_POLYGON);//HOUSE C
+		glBegin(GL_POLYGON);
 			glVertex2f(255,302.5);
 			glVertex2f(275.0,302.5);
 			glVertex2f(275.0,312.5);
 			glVertex2f(255.0,312.5);
 			glEnd();
 
-		glBegin(GL_POLYGON);//HOUSE D
+		glBegin(GL_POLYGON);
 			glVertex2f(255,320.0);
 			glVertex2f(275.0,320.0);
 			glVertex2f(275.0,330.0);
 			glVertex2f(255.0,330.0);
 			glEnd();
 
-		glBegin(GL_POLYGON);//HOUSE E
+		glBegin(GL_POLYGON);
 			glVertex2f(285,267.5);
 			glVertex2f(295.0,267.5);
 			glVertex2f(295.0,277.5);
 			glVertex2f(285.0,277.5);
 			glEnd();
 
-		glBegin(GL_POLYGON);//HOUSE F
+		glBegin(GL_POLYGON);
 			glVertex2f(285,285.0);
 			glVertex2f(295.0,285);
 			glVertex2f(295.0,295);
 			glVertex2f(285.0,295);
 			glEnd();
 
-		glBegin(GL_POLYGON);//HOUSE G
+		glBegin(GL_POLYGON);
 			glVertex2f(285,302.5);
 			glVertex2f(295.0,302.5);
 			glVertex2f(295.0,312.5);
 			glVertex2f(285.0,312.5);
 			glEnd();
 
-		glBegin(GL_POLYGON);//HOUSE H
+		glBegin(GL_POLYGON);
 			glVertex2f(285,320.0);
 			glVertex2f(295.0,320.0);
 			glVertex2f(295.0,330.0);
 			glVertex2f(285.0,330.0);
 			glEnd();
 			glColor3f(0.647059 ,0.164706  ,0.164706);
-			glBegin(GL_POLYGON);//solid cone
+			glBegin(GL_POLYGON);
 			glVertex2f(26,250);
 			glVertex2f(52,250);
 			glVertex2f(39,290);
@@ -288,7 +288,7 @@ flag=1;
 			semicircle(20.0,50,300);
 
 	glColor3f(0.0,0.0 ,0.0);
-			glBegin(GL_LINES);//wires
+			glBegin(GL_LINES);
 			glVertex2f(37,313);
 			glVertex2f(62,310);
 			glVertex2f(63,287);
@@ -300,7 +300,7 @@ flag=1;
 		glPointSize(2.0);
 
 	glColor3f(1.0,1.0 ,1.0);
-			glBegin(GL_POINTS);//road paint
+			glBegin(GL_POINTS);
 			glVertex2f(497,56);
 			glVertex2f(488,65);
 			glVertex2f(479,74);
@@ -328,7 +328,7 @@ flag=1;
 			glEnd();
 
 
-	glColor3f(0.0,0.0,0.0);//stand object
+	glColor3f(0.0,0.0,0.0);
 	glBegin(GL_POLYGON);
 	glVertex2f(130,10.0);
 	glVertex2f(160,10.0);
@@ -344,7 +344,7 @@ flag=1;
 	glEnd();
 
     glColor3f(0.0f, 0.1f, 0.1f);
-	glBegin(GL_POLYGON);//core
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,40.0+i);
 		glVertex2f(262.5,40.0+i);
 		glVertex2f(262.5,140.0+i);
@@ -353,16 +353,16 @@ flag=1;
 
 	glEnd();
 
-	glColor3f(1.0,1.0,1.0);//bonnet
-	glBegin(GL_POLYGON);//front
+	glColor3f(1.0,1.0,1.0);
+	glBegin(GL_POLYGON);
 	glVertex2f(237.5,140.0+i);
 	glVertex2f(262.5,140.0+i);
 	glVertex2f(250,190.0+i);
 	glEnd();
 
-	//glColor3f(11, 61, 145);
+
 	glColor3f(0.0f, 0.1f, 0.1f);
-	glBegin(GL_POLYGON);//core
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,20.0+i);
 		glVertex2f(262.5,20.0+i);
 		glVertex2f(262.5,120.0+i);
@@ -373,45 +373,45 @@ flag=1;
 
 
 	glColor3f(1.0,0.0,0.0);
-	glBegin(GL_POLYGON);//left_side_top
+	glBegin(GL_POLYGON);
 	glVertex2f(237.5,140.0+i);
 	glVertex2f(217.5,115.0+i);
 	glVertex2f(237.5,115.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//left_side_bottom
+		glBegin(GL_POLYGON);
 	glVertex2f(237.5,20.0+i);
 	glVertex2f(217.5,20.0+i);
 	glVertex2f(237.5,70.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//right_side_bottom
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,20.0+i);
 	glVertex2f(282.5,20.0+i);
 	glVertex2f(262.5,70.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//right_side_top
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,140.0+i);
 	glVertex2f(262.5,115.0+i);
 	glVertex2f(282.5,115.0+i);
 	glEnd();
 	glColor3f(0.556863 ,0.137255  ,0.419608);
-		glBegin(GL_POLYGON);//bottom_1_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(237.5,20.0+i);
 	glVertex2f(244.5,20.0+i);
 	glVertex2f(241,0.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//bottom_2_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(246.5,20.0+i);
 	glVertex2f(253.5,20.0+i);
 	glVertex2f(249.5,0.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//bottom_3_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,20.0+i);
 	glVertex2f(255.5,20.0+i);
 	glVertex2f(258.5,0.0+i);
 	glEnd();
 
 
-	glBegin(GL_POLYGON);//left_stand_holder
+	glBegin(GL_POLYGON);
 	glVertex2f(182.5,85.0);
 	glVertex2f(182.5,0.0);
 	glVertex2f(187.5,0.0);
@@ -421,7 +421,7 @@ flag=1;
 	glVertex2f(182.5,85.0);
 	glEnd();
 	glBegin(GL_POLYGON);
-glVertex2f(312.5,85.0);//right_stand_holder
+glVertex2f(312.5,85.0);
 	glVertex2f(312.5,0.0);
 	glVertex2f(307.5,0.0);
 	glVertex2f(307.5,80.0);
@@ -450,7 +450,7 @@ for(i=100;i<=500;i++)
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 	glColor3f(0.0f, 0.1f, 0.1f);
-	glBegin(GL_POLYGON);//core
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,40.0+i);
 		glVertex2f(262.5,40.0+i);
 		glVertex2f(262.5,140.0+i);
@@ -459,16 +459,16 @@ for(i=100;i<=500;i++)
 
 	glEnd();
 
-	glColor3f(1.0,1.0,1.0);//bonnet
-	glBegin(GL_POLYGON);//front
+	glColor3f(1.0,1.0,1.0);
+	glBegin(GL_POLYGON);
 	glVertex2f(237.5,140.0+i);
 	glVertex2f(262.5,140.0+i);
 	glVertex2f(250,190.0+i);
 	glEnd();
 
-	//glColor3f(11, 61, 145);
+
 	glColor3f(0.0f, 0.1f, 0.1f);
-	glBegin(GL_POLYGON);//core
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,20.0+i);
 		glVertex2f(262.5,20.0+i);
 		glVertex2f(262.5,120.0+i);
@@ -478,38 +478,38 @@ for(i=100;i<=500;i++)
 	glEnd();
 
 	glColor3f(1.0,0.0,0.0);
-	glBegin(GL_POLYGON);//left_side_top
+	glBegin(GL_POLYGON);
 	glVertex2f(237.5,140.0+i);
 	glVertex2f(217.5,115.0+i);
 	glVertex2f(237.5,115.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//left_side_bottom
+		glBegin(GL_POLYGON);
 	glVertex2f(237.5,20.0+i);
 	glVertex2f(217.5,20.0+i);
 	glVertex2f(237.5,70.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//right_side_bottom
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,20.0+i);
 	glVertex2f(282.5,20.0+i);
 	glVertex2f(262.5,70.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//right_side_top
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,140.0+i);
 	glVertex2f(262.5,115.0+i);
 	glVertex2f(282.5,115.0+i);
 	glEnd();
 	glColor3f(0.556863 ,0.137255  ,0.419608);
-		glBegin(GL_POLYGON);//bottom_1_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(237.5,20.0+i);
 	glVertex2f(244.5,20.0+i);
 	glVertex2f(241,0.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//bottom_2_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(246.5,20.0+i);
 	glVertex2f(253.5,20.0+i);
 	glVertex2f(249.5,0.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//bottom_3_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,20.0+i);
 	glVertex2f(255.5,20.0+i);
 	glVertex2f(258.5,0.0+i);
@@ -521,7 +521,7 @@ for(i=100;i<=500;i++)
 				 else
 					 glColor3f(1.0,0.816,0.0);
 
-				 glBegin(GL_POLYGON);//outer fume
+				 glBegin(GL_POLYGON);
 		glVertex2f(237.5,20+i);
 		glVertex2f(234.16,16.66+i);
 		glVertex2f(230.82,13.32+i);
@@ -549,7 +549,7 @@ for(i=100;i<=500;i++)
 		glVertex2f(274.5,8+i);
 		glVertex2f(270.5,12+i);
 		glVertex2f(266.5,16+i);
-		glVertex2f(262.5,20+i);//28 points
+		glVertex2f(262.5,20+i);
 		glEnd();
 
 				 	if((p%2)==0)
@@ -557,7 +557,7 @@ for(i=100;i<=500;i++)
 				 else
 					 glColor3f(1.0,0.25,0.0);
 
-		glBegin(GL_POLYGON);//inner fume
+		glBegin(GL_POLYGON);
 		glVertex2f(237.5,20+i);
 		glVertex2f(236.5,17.5+i);
 		glVertex2f(235.5,15+i);
@@ -578,7 +578,7 @@ for(i=100;i<=500;i++)
 		glVertex2f(265.5,12.5+i);
 		glVertex2f(264.5,15+i);
 		glVertex2f(263.5,17.5+i);
-		glVertex2f(262.5,20+i);//21 points
+		glVertex2f(262.5,20+i);
 
 		glEnd();
 		p=p+1;
@@ -623,14 +623,14 @@ for(i=195;i<=200;i++)
 
 	 if(count<=130){
 	glColor3f(0.0f, 0.1f, 0.1f);
-	glBegin(GL_POLYGON);//core
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,40.0+i);
 		glVertex2f(262.5,40.0+i);
 		glVertex2f(262.5,140.0+i);
 		glVertex2f(237.5,140.0+i);
 	glEnd();
 	glColor3f(0.0f, 0.1f, 0.1f);
-	glBegin(GL_POLYGON);//core
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,20.0+i);
 		glVertex2f(262.5,20.0+i);
 		glVertex2f(262.5,120.0+i);
@@ -643,15 +643,15 @@ for(i=195;i<=200;i++)
 
 	if(count>=150){
 	static int k = i;
-	glColor3f(1.0,1.0,1.0);//satellite
-	glBegin(GL_POLYGON);//core
+	glColor3f(1.0,1.0,1.0);
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,150.0+k);
 		glVertex2f(252.5,150.0+k);
 		glVertex2f(252.5,120.0+k);
 		glVertex2f(237.5,120.0+k);
 	glEnd();
 
-	glBegin(GL_POLYGON);//side-panels
+	glBegin(GL_POLYGON);
 		glVertex2f(237.5,140.0+k);
 		glVertex2f(230,140.0+k);
 		glVertex2f(230,130.0+k);
@@ -665,8 +665,8 @@ for(i=195;i<=200;i++)
 	}
 
 	else{
-	glColor3f(1.0,1.0,1.0);//bonnet
-	glBegin(GL_POLYGON);//front
+	glColor3f(1.0,1.0,1.0);
+	glBegin(GL_POLYGON);
 	glVertex2f(237.5,140.0+i);
 	glVertex2f(262.5,140.0+i);
 	glVertex2f(250,190.0+i);
@@ -676,22 +676,22 @@ for(i=195;i<=200;i++)
 
 	if(count<=120){
 	glColor3f(1.0,0.0,0.0);
-	glBegin(GL_POLYGON);//left_side_top
+	glBegin(GL_POLYGON);
 	glVertex2f(237.5,140.0+i);
 	glVertex2f(217.5,115.0+i);
 	glVertex2f(237.5,115.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//left_side_bottom
+		glBegin(GL_POLYGON);
 	glVertex2f(237.5,20.0+i);
 	glVertex2f(217.5,20.0+i);
 	glVertex2f(237.5,70.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//right_side_bottom
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,20.0+i);
 	glVertex2f(282.5,20.0+i);
 	glVertex2f(262.5,70.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//right_side_top
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,140.0+i);
 	glVertex2f(262.5,115.0+i);
 	glVertex2f(282.5,115.0+i);
@@ -700,17 +700,17 @@ for(i=195;i<=200;i++)
 
 	if(count<=110){
 	glColor3f(0.556863 ,0.137255  ,0.419608);
-		glBegin(GL_POLYGON);//bottom_1_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(237.5,20.0+i);
 	glVertex2f(244.5,20.0+i);
 	glVertex2f(241,0.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//bottom_2_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(246.5,20.0+i);
 	glVertex2f(253.5,20.0+i);
 	glVertex2f(249.5,0.0+i);
 	glEnd();
-		glBegin(GL_POLYGON);//bottom_3_exhaust
+		glBegin(GL_POLYGON);
 	glVertex2f(262.5,20.0+i);
 	glVertex2f(255.5,20.0+i);
 	glVertex2f(258.5,0.0+i);
@@ -737,7 +737,7 @@ void mars(float radius)
       float degInRad = i*DEG2RAD;
       glVertex2f(300+f+cos(degInRad)*radius,500-t+(sin(degInRad))
 
-*radius);//100,100 specifies centre of the circle
+*radius);
    }
 
    glEnd();
@@ -745,7 +745,7 @@ void mars(float radius)
    f=f+0.1;
 }
 
-//keys that trigger manual Lanch
+
 void keyboard(unsigned char key, int x, int y)
 {
 	if (key == 'I' || key == 'i'){
@@ -781,7 +781,7 @@ void page()
 
 
 	drawlargestring(125, 420, "ANIMATION DEPICTING A ROCKETSHIP TAKEOFF! ");
-    //Underline to the heading
+
 	glBegin(GL_LINES);
         glVertex2d(125,415);
         glVertex2d(310,415);
@@ -856,7 +856,7 @@ void display()
 void myinit()
 {
 	glClearColor(1 ,0.6 ,0.2,1.0);
-   // glClearColor(51,193,193,1);
+
 	glPointSize(1.0);
 	gluOrtho2D(0.0,499.0,0.0,499.0);
 }
